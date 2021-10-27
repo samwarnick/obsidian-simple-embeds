899e2bd74c1691f8e67f5130c9a2844d027dcee8
Sam Warnick <sam.t.warnick@gmail.com>
2021-10-27 19:38:54 -0400

    Update regex

--- embeds/instagram.ts
+++ embeds/instagram.ts
@@ -2,7 +2,7 @@ import { EmbedSource } from "./";
 import { PluginSettings } from "settings";
 
 const INSTAGRAM_LINK = new RegExp(
-  /https:\/\/www\.instagram\.com\/(p|tv)\/(\w+)/
+  /https:\/\/www\.instagram\.com\/p\/(\w+)/
 );
 
 interface Instagram {
