51210fd0c8978563559fb662b705ad9ae9d4af74
Chardinson Ventura <36754778+chardinson@users.noreply.github.com>
2023-07-10 15:14:46 -0400

    Update Reddit Embed
    Updated the embed to use the embed provided by Reddit.

--- embeds/reddit.ts
+++ embeds/reddit.ts
@@ -2,14 +2,10 @@ import { Setting } from "obsidian";
 import { PluginSettings } from "settings";
 import { EmbedSource, EnableEmbedKey } from "./";
 
-const REDDIT_LINK = new RegExp(
-  /https:\/\/(?:www\.)?reddit\.com\/r\/(?<comment>.+)/,
-);
-
 export class RedditEmbed implements EmbedSource {
   name = "Reddit";
   enabledKey: EnableEmbedKey = "replaceRedditLinks";
-  regex = REDDIT_LINK;
+  regex = /https:\/\/(?:www\.)?reddit\.com\/r\/(?<comment>.+)/;
 
   createEmbed(
     link: string,
@@ -17,40 +13,50 @@ export class RedditEmbed implements EmbedSource {
     settings: Readonly<PluginSettings>,
     currentTheme: "light" | "dark",
   ) {
-    const iframe = document.createElement("iframe");
+    const blockquote = document.createElement("blockquote");
+    blockquote.classList.add("reddit-embed-bq");
+    blockquote.style.height = "500px";
+    blockquote.setAttribute("data-embed-height", "500");
+    blockquote.setAttribute("data-embed-locale", settings.redditLocale || "auto");
+    blockquote.setAttribute("data-embed-showmedia", settings.redditHideMedia ? "false" : "true");
+    blockquote.setAttribute("data-embed-theme", settings.redditTheme === "dark" ? "dark" : "light");
+    blockquote.setAttribute("data-embed-showedits", settings.redditHideEdits ? "false" : "true");
+    blockquote.setAttribute("data-embed-showusername", settings.redditHideUsername ? "false" : "true");
+    blockquote.setAttribute("data-embed-created", new Date().toISOString());
 
-    const comment = link.match(REDDIT_LINK).groups.comment;
+    const aLink = document.createElement("a");
+    aLink.href = link;
+    aLink.textContent = link;
+    blockquote.appendChild(aLink);
 
-    const theme =
-      settings.redditTheme === "auto" ? currentTheme : settings.redditTheme;
-    iframe.src = `https://www.redditmedia.com/r/${comment}/?ref_source=embed&amp;ref=share&amp;embed=true;theme=${theme}`;
-    iframe.setAttribute("frameborder", "0");
-    iframe.setAttribute(
-      "sandbox",
-      "allow-scripts allow-same-origin allow-popups",
-    );
-    iframe.height = "198";
-    iframe.width = "640";
-    container.appendChild(iframe);
-    container.classList.add("reddit");
+    if (settings.redditTheme === "dark") {
+      blockquote.setAttribute("data-embed-theme", "dark");
+    } else if (currentTheme === "dark") {
+      blockquote.setAttribute("data-embed-theme", "dark");
+    }
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.reddit.com/widgets.js";
+    script.setAttribute("charset", "UTF-8");
+
+    container.appendChild(blockquote);
+    container.appendChild(script);
     return container;
   }
 
   updateTheme(theme: "dark" | "light", settings: Readonly<PluginSettings>) {
-    if (settings.redditTheme !== "auto") {
-      return;
-    }
-    const redditEmbed = document.querySelectorAll(
-      ".embed-container.reddit iframe",
-    ) as NodeListOf<HTMLIFrameElement>;
-    redditEmbed.forEach((embed) => {
-      let src = embed.src;
-      if (theme === "dark") {
-        src = src.replace("theme=light", "theme=dark");
+    const redditEmbeds = document.querySelectorAll(
+      ".embed-container.reddit blockquote.reddit-embed-bq"
+    );
+    redditEmbeds.forEach((blockquote) => {
+      if (settings.redditTheme === "dark") {
+        blockquote.setAttribute("data-embed-theme", "dark");
+      } else if (theme === "dark") {
+        blockquote.setAttribute("data-embed-theme", "dark");
       } else {
-        src = src.replace("theme=dark", "theme=light");
+        blockquote.removeAttribute("data-embed-theme");
       }
-      embed.src = src;
     });
   }
 
@@ -59,21 +65,78 @@ export class RedditEmbed implements EmbedSource {
     settings: Readonly<PluginSettings>,
     saveSettings: (updates: Partial<PluginSettings>) => Promise<void>,
   ) {
-    const themeSetting = new Setting(containerEl)
-      .setName("Theme")
+    const settingsList: Setting[] = [];
+
+    // Language setting
+    const languageSetting = new Setting(containerEl)
+      .setName("Select Display Text Language")
       .addDropdown((dropdown) => {
         dropdown
           .addOptions({
             auto: "Automatic",
-            dark: "Dark",
-            light: "Light",
+            "en-EN": "English",
+            "fr-FR": "French",
+            "de-DE": "German",
+            "es-MX": "Mexican Spanish",
+            "es-ES": "Spain Spanish",
+            "pt-BR": "Brazilian Portuguese",
+            "pt-PT": "Portugal Portuguese",
           })
-          .setValue(settings.redditTheme)
-          .onChange(async (value: "auto" | "dark" | "light") => {
-            await saveSettings({ redditTheme: value });
+          .setValue(settings.redditLocale || "auto")
+          .onChange(async (value: string) => {
+            await saveSettings({ redditLocale: value });
           });
       });
+    settingsList.push(languageSetting);
 
-    return [themeSetting];
+    // Hide post content setting
+    const hidePostContentSetting = new Setting(containerEl)
+      .setName("Hide Post Content")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditHideMedia || false)
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditHideMedia: value });
+          });
+      });
+    settingsList.push(hidePostContentSetting);
+    
+    // Dark mode setting
+    const darkModeSetting = new Setting(containerEl)
+      .setName("Display in Dark Mode")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditTheme === "dark")
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditTheme: value ? "dark" : "auto" });
+          });
+      });
+    settingsList.push(darkModeSetting);
+
+    // Hide if edited setting
+    const hideIfEditedSetting = new Setting(containerEl)
+      .setName("Hide If Edited")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditHideEdits || false)
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditHideEdits: value });
+          });
+      });
+    settingsList.push(hideIfEditedSetting);
+
+    // Hide username setting
+    const hideUsernameSetting = new Setting(containerEl)
+      .setName("Hide Username")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditHideUsername || false)
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditHideUsername: value });
+          });
+      });
+    settingsList.push(hideUsernameSetting);
+
+    return settingsList;
   }
 }
