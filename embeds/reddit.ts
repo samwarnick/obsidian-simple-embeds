307fc8186f79fec77053eba870db585e0bf310dc
Chardinson Ventura <36754778+chardinson@users.noreply.github.com>
2023-09-12 19:11:19 -0400

    Update Reddit Embed (#71)
    * Update Reddit Embed
    
    Updated the embed to use the embed provided by Reddit.
    
    * Small changes
    
    * Error load embeds
    
    * Add settings Reddit embed in settings file

--- embeds/reddit.ts
+++ embeds/reddit.ts
@@ -3,77 +3,155 @@ import { PluginSettings } from "settings";
 import { EmbedSource, EnableEmbedKey } from "./";
 
 const REDDIT_LINK = new RegExp(
-  /https:\/\/(?:www\.)?reddit\.com\/r\/(?<comment>.+)/,
+  /https:\/\/(?:www\.)?reddit\.com\/r\/(?<comment>.+)/
 );
 
 export class RedditEmbed implements EmbedSource {
   name = "Reddit";
   enabledKey: EnableEmbedKey = "replaceRedditLinks";
   regex = REDDIT_LINK;
+  blockquotes: any;
 
   createEmbed(
     link: string,
     container: HTMLElement,
     settings: Readonly<PluginSettings>,
-    currentTheme: "light" | "dark",
+    currentTheme: "light" | "dark"
   ) {
-    const iframe = document.createElement("iframe");
-
-    const comment = link.match(REDDIT_LINK).groups.comment;
-
-    const theme =
-      settings.redditTheme === "auto" ? currentTheme : settings.redditTheme;
-    iframe.src = `https://www.redditmedia.com/r/${comment}/?ref_source=embed&amp;ref=share&amp;embed=true;theme=${theme}`;
-    iframe.setAttribute("frameborder", "0");
-    iframe.setAttribute(
-      "sandbox",
-      "allow-scripts allow-same-origin allow-popups",
+    const theme = settings.redditTheme === "dark" ? "dark" : "light";
+    const blockquote = document.createElement("blockquote");
+    blockquote.classList.add("reddit-embed-bq");
+    blockquote.setAttribute("data-embed-height", "500");
+    blockquote.setAttribute(
+      "data-embed-locale",
+      settings.redditLocale || "auto"
     );
-    iframe.height = "198";
-    iframe.width = "640";
-    container.appendChild(iframe);
-    container.classList.add("reddit");
-    return container;
+    blockquote.setAttribute(
+      "data-embed-showmedia",
+      settings.redditHideMedia ? "false" : "true"
+    );
+    blockquote.setAttribute(
+      "data-embed-theme",
+      theme
+    );
+    blockquote.setAttribute(
+      "data-embed-showedits",
+      settings.redditHideEdits ? "false" : "true"
+    );
+    blockquote.setAttribute(
+      "data-embed-showusername",
+      settings.redditHideUsername ? "false" : "true"
+    );
+    blockquote.setAttribute("data-embed-created", new Date().toISOString());
+
+    const aLink = document.createElement("a");
+    aLink.href = link;
+    aLink.textContent = link;
+    blockquote.appendChild(aLink);
+
+    if (theme === "dark" || currentTheme === "dark") {
+      blockquote.setAttribute("data-embed-theme", "dark");
+    }
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://embed.reddit.com/widgets.js`;
+    script.setAttribute("charset", "UTF-8");
+
+    const newContainer = document.createElement("div");
+    newContainer.appendChild(blockquote);
+    newContainer.appendChild(script);
+
+    container.replaceWith(newContainer);
+    return newContainer;
   }
 
   updateTheme(theme: "dark" | "light", settings: Readonly<PluginSettings>) {
-    if (settings.redditTheme !== "auto") {
-      return;
-    }
-    const redditEmbed = document.querySelectorAll(
-      ".embed-container.reddit iframe",
-    ) as NodeListOf<HTMLIFrameElement>;
-    redditEmbed.forEach((embed) => {
-      let src = embed.src;
-      if (theme === "dark") {
-        src = src.replace("theme=light", "theme=dark");
-      } else {
-        src = src.replace("theme=dark", "theme=light");
-      }
-      embed.src = src;
+    this.blockquotes.forEach((blockquote: { classList: { toggle: (arg0: string, arg1: boolean) => void; }; }) => {
+      blockquote.classList.toggle(
+        "dark",
+        settings.redditTheme === "dark" || theme === "dark"
+      );
     });
   }
 
   createAdditionalSettings(
     containerEl: HTMLElement,
     settings: Readonly<PluginSettings>,
-    saveSettings: (updates: Partial<PluginSettings>) => Promise<void>,
+    saveSettings: (updates: Partial<PluginSettings>) => Promise<void>
   ) {
-    const themeSetting = new Setting(containerEl)
-      .setName("Theme")
+    const settingsList: Setting[] = [];
+
+    // Language setting
+    const languageSetting = new Setting(containerEl)
+      .setName("Select Display Text Language")
       .addDropdown((dropdown) => {
         dropdown
           .addOptions({
             auto: "Automatic",
-            dark: "Dark",
-            light: "Light",
+            "en-EN": "English",
+            "fr-FR": "French",
+            "de-DE": "German",
+            "es-MX": "Mexican Spanish",
+            "es-ES": "Spain Spanish",
+            "pt-BR": "Brazilian Portuguese",
+            "pt-PT": "Portugal Portuguese",
           })
-          .setValue(settings.redditTheme)
-          .onChange(async (value: "auto" | "dark" | "light") => {
-            await saveSettings({ redditTheme: value });
+          .setValue(settings.redditLocale || "auto")
+          .onChange(async (value: string) => {
+            await saveSettings({ redditLocale: value });
           });
       });
+    settingsList.push(languageSetting);
 
-    return [themeSetting];
+    // Hide post content setting
+    const hidePostContentSetting = new Setting(containerEl)
+      .setName("Hide Post Content")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditHideMedia || false)
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditHideMedia: value });
+          });
+      });
+    settingsList.push(hidePostContentSetting);
+
+    // Dark mode setting
+    const darkModeSetting = new Setting(containerEl)
+      .setName("Display in Dark Mode")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditTheme === "dark")
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditTheme: value ? "dark" : "auto" });
+          });
+      });
+    settingsList.push(darkModeSetting);
+
+    // Hide if edited setting
+    const hideIfEditedSetting = new Setting(containerEl)
+      .setName("Hide If Edited")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditHideEdits || false)
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditHideEdits: value });
+          });
+      });
+    settingsList.push(hideIfEditedSetting);
+
+    // Hide username setting
+    const hideUsernameSetting = new Setting(containerEl)
+      .setName("Hide Username")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(settings.redditHideUsername || false)
+          .onChange(async (value: boolean) => {
+            await saveSettings({ redditHideUsername: value });
+          });
+      });
+    settingsList.push(hideUsernameSetting);
+
+    return settingsList;
   }
 }
